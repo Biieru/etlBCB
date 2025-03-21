4df0682b59b5499c57c92d7f2a59b2b26f4868f3
Gabriel C. Araujo <48807040+Biieru@users.noreply.github.com>
2025-03-21 16:08:17 -0300

    Primeiro Commit²

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+import pandas as pd 
+
+
+def requestApiBcd (data):
+    url = f"https://olinda.bcb.gov.br/olinda/servico/MPV_DadosAbertos/versao/v1/odata/MeiosdePagamentosTrimestralDA(trimestre=@trimestre)?@trimestre=%27{data}27&$format=json"
+
+    req = requests.get(url)
+    dados = req.json()
+
+    df = pd.json_normalize(dados['value'])
+    return print (df)
+
+requestApiBcd('20241')
