51581979d89550e3b297c15c7a813957a94d74b2
Gabriel C. Araujo <48807040+Biieru@users.noreply.github.com>
2025-03-29 00:39:48 -0300

    Update load.py

--- src/load.py
+++ src/load.py
@@ -1,5 +1,21 @@
 import pandas as pd
 
 def salvarCsv(df: pd.DataFrame, nome_arquivo: str, separador: str = ';', decimal: str = ',') -> None:
+    """
+    Salva o DataFrame em um arquivo CSV.
+
+    Parâmetros:
+    -----------
+    df : pd.DataFrame
+        O DataFrame a ser salvo.
+    nome_arquivo : str
+        O nome ou caminho do arquivo onde o DataFrame será salvo.
+    separador : str, opcional
+        O separador a ser usado no arquivo CSV (padrão é ';').
+    decimal : str, opcional
+        O caractere utilizado para representar decimais (padrão é ',').
+
+        PS: Esta função não retorna nada, ela apenas salva o DataFrame em um arquivo CSV.
+    """
     df.to_csv(nome_arquivo, sep=separador, decimal=decimal)
     return
