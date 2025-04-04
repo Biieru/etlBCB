c4dad5b3b71b4e4b9907110ff914bae806d45583
Gabriel C. Araujo <48807040+Biieru@users.noreply.github.com>
2025-04-04 15:51:38 -0300

    Update load.py

--- src/load.py
+++ src/load.py
@@ -1,6 +1,11 @@
 import pandas as pd
+import sqlite3
+from sqlalchemy import create_engine
 
-def salvarCsv(df: pd.DataFrame, nome_arquivo: str, separador: str = ';', decimal: str = ',') -> None:
+
+def salvarCsv(
+    df: pd.DataFrame, nome_arquivo: str, separador: str = ";", decimal: str = ","
+) -> None:
     """
     Salva o DataFrame em um arquivo CSV.
 
@@ -18,4 +23,27 @@ def salvarCsv(df: pd.DataFrame, nome_arquivo: str, separador: str = ';', decimal
         PS: Esta função não retorna nada, ela apenas salva o DataFrame em um arquivo CSV.
     """
     df.to_csv(nome_arquivo, sep=separador, decimal=decimal)
+    return
+
+
+def salvarSQLite(df: pd.DataFrame, nome_banco: str, nome_tabela: str):
+
+    conn = sqlite3.connect(nome_banco)
+
+    df.to_sql(nome_tabela, conn, if_exists="replace", index=False)
+
+    conn.close()
+    return
+
+
+def salvarMySQL(
+    df: pd.DataFrame,
+    usuario: str,
+    senha: str,
+    host: str,
+    nome_banco: str,
+    nome_tabela: str,
+):
+    engine = create_engine(f"mysql+pymysql://{usuario}:{senha}@{host}/{nome_banco}")
+    df.to_sql(nome_tabela, con=engine, if_exists="replace", index=False)
     return
