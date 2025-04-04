fea50e6c8dcf041e5d88b5dac33aa0ae7c36c15e
Gabriel C. Araujo <48807040+Biieru@users.noreply.github.com>
2025-04-04 15:50:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pandas as pd
 from src.extractTransform import requestApiBcb
-from src.load import salvarCsv
+from src.load import salvarCsv, salvarSQLite, salvarMySQL
 
-dadosBcb = requestApiBcb('20191')
-salvarCsv(dadosBcb, "etlBCB/src/datasets/meiosPagamentosTri.csv", ";", ".")
+dadosBcb = requestApiBcb("20191")
+# salvarCsv(dadosBcb, "C:/Users/Aluno/Documents/TADS034/etlBCB-main/src/datasets/meiosPagamentosTri.csv", ";", ".")
 
+# salvarSQLite(dadosBcb, "C:/Users/Aluno/Documents/TADS034/etlBCB-main/src/datasets/etlBCB.db", "meios_pagamenos_tri")
+
+salvarMySQL(dadosBcb, "root", "teste", "localhost", "etlbcb", "meios_pagamentos_tri")
