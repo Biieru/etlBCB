a70637fff700c6872eadda18bc2ccb815c80e54b
varley852 <varleymarques852@gmail.com>
2025-04-11 16:26:57 -0300

    Atualizando aula do dia 11

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from src.load import salvarCsv, salvarSQLite, salvarMySQL
 dadosBcb = requestApiBcb("20191")
 # salvarCsv(dadosBcb, "C:/Users/Aluno/Documents/TADS034/etlBCB-main/src/datasets/meiosPagamentosTri.csv", ";", ".")
 
-# salvarSQLite(dadosBcb, "C:/Users/Aluno/Documents/TADS034/etlBCB-main/src/datasets/etlBCB.db", "meios_pagamenos_tri")
+salvarSQLite(dadosBcb, "src/datasets/etlBCB.db", "meios_pagamentos_tri")
 
-salvarMySQL(dadosBcb, "root", "teste", "localhost", "etlbcb", "meios_pagamentos_tri")
+#salvarMySQL(dadosBcb, "root", "teste", "localhost", "etlbcb", "meios_pagamentos_tri")
