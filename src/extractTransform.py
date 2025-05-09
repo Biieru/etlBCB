a3287d2c10ad424a60a974509770f694f7af35bc
Gabriel C. Araujo <48807040+Biieru@users.noreply.github.com>
2025-05-09 16:08:16 -0300

    Aula do dia 09

--- src/extractTransform.py
+++ src/extractTransform.py
@@ -2,9 +2,15 @@ import requests
 import pandas as pd
 
 
-def requestApiBcb(data):
+def requestApiBcb(data: str) -> pd.DataFrame:
     """
     Função para extrair os dados da API do Banco Central.
+
+    Parâmetros:
+    data - string - AAAAT (Exemplo: 20191)
+
+    Saída:
+    DataFrame - Estrutura de dados do pandas
     """
     url = f"https://olinda.bcb.gov.br/olinda/servico/MPV_DadosAbertos/versao/v1/odata/MeiosdePagamentosTrimestralDA(trimestre=@trimestre)?@trimestre=%27{data}%27&$format=json"
     req = requests.get(url)
--- src/load.py
+++ src/load.py
@@ -3,40 +3,19 @@ import sqlite3
 from sqlalchemy import create_engine
 
 
-def salvarCsv(
-    df: pd.DataFrame, nome_arquivo: str, separador: str = ";", decimal: str = ","
-) -> None:
-    """
-    Salva o DataFrame em um arquivo CSV.
-
-    Parâmetros:
-    -----------
-    df : pd.DataFrame
-        O DataFrame a ser salvo.
-    nome_arquivo : str
-        O nome ou caminho do arquivo onde o DataFrame será salvo.
-    separador : str, opcional
-        O separador a ser usado no arquivo CSV (padrão é ';').
-    decimal : str, opcional
-        O caractere utilizado para representar decimais (padrão é ',').
-
-        PS: Esta função não retorna nada, ela apenas salva o DataFrame em um arquivo CSV.
-    """
-    df.to_csv(nome_arquivo, sep=separador, decimal=decimal)
+def salvarCsv(df: pd.DataFrame, nome_arquivo: str, separador: str, dec: str):
+    df.to_csv(nome_arquivo, sep=separador, decimal=dec)
     return
 
 
-def salvarSQLite(df: pd.DataFrame, nome_banco: str, nome_tabela: str):
-
+def salvarSqlite(df: pd.DataFrame, nome_banco: str, nome_tabela):
     conn = sqlite3.connect(nome_banco)
-
     df.to_sql(nome_tabela, conn, if_exists="replace", index=False)
-
     conn.close()
     return
 
 
-def salvarMySQL(
+def salvarMySql(
     df: pd.DataFrame,
     usuario: str,
     senha: str,
